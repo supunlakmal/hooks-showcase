d8ef88313ffb65bc9c537c8988f4108f7f1b7894
Supun Lakmal Abesekara <supunlakmal61@gmail.com>
2025-04-18 03:08:54 +0000

    bump version to 2.2.0 and remove unused authentication pages and API routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import "../styles/index.css";
 import "../styles/prism-vsc-dark-plus.css";
-import ToasterContext from "./api/contex/ToasetContex";
+
 import { useEffect, useState } from "react";
 import PreLoader from "@/components/Common/PreLoader";
 
@@ -34,19 +34,19 @@ export default function RootLayout({
         {loading ? (
           <PreLoader />
         ) : (
-          <SessionProvider>
-            <ThemeProvider
-              attribute="class"
-              enableSystem={false}
-              defaultTheme="light"
-            >
-              <ToasterContext />
-              <Header />
-              {children}
-              <Footer />
-              <ScrollToTop />
-            </ThemeProvider>
-          </SessionProvider>
+
+          <ThemeProvider
+            attribute="class"
+            enableSystem={false}
+            defaultTheme="light"
+          >
+
+            <Header />
+            {children}
+            <Footer />
+            <ScrollToTop />
+          </ThemeProvider>
+
         )}
       </body>
     </html>
